4eb1f334688906af55f4be34f5aba3674db110cc
Bklepitsch <bklepitsch@mail.bradley.edu>
2023-03-22 23:42:38 -0500

    fixes to validate.js

--- validate.js
+++ validate.js
@@ -7,9 +7,19 @@ function validateForm(event) {
     myForm.completion.style.backgroundColor = "LightGreen";
     myForm.details.style.backgroundColor = "LightGreen";
     
-    if (myForm.name.value.isempty() ) {
+    if (myForm.name.value.length == 0) {
        myForm.name.style.backgroundColor = "Orange";
     }
+    if (myForm.videogame.value.length == 0) {
+      myForm.videogame.style.backgroundColor = "Orange";
+   }
+    if (myForm.rating.value.length == 0) {
+      myForm.rating.style.backgroundColor = "Orange";
+    }
+    if (myForm.completion.value.length == 0) {
+      myForm.completion.style.backgroundColor = "Orange";
+    }
+
  }
  
  let myForm = document.querySelector("#myForm");
